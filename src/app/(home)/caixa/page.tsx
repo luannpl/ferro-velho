cf82e344438ed7952e832834dce350243541c45b
Ronaldo-j <ronaldo00filho@gmail.com>
2025-12-06 19:25:49 -0300

    feat: add cash register control with open/close state and introduce Card component

--- src/app/(home)/caixa/page.tsx
+++ src/app/(home)/caixa/page.tsx
@@ -1,18 +1,33 @@
+'use client'
 import { Button } from "@/components/ui/button";
-
+import { useState } from "react";
 export default function Caixa() {
+    const [open, setOpen] = useState(false);
     return (
-        <body>
-            <main>
-                <section className="flex flex-col gap-4">
-                    <h1>Historico de Caixa</h1>
+        <main>
+            <section className="flex flex-col gap-6 p-6 bg-white/30 border border-white/20 rounded-xl backdrop-blur-lg shadow-lg">
+                <div>
+                    <div className="text-lg font-semibold">Histórico de Caixas</div>
+                    <p className="text-muted-foreground text-sm">x caixas anteriores</p>
+                </div>
+            </section>
 
-                </section>
-                <section>
-                    <Button className="bg-green-500">Abrir Caixa</Button>
-                    
-                </section>
-            </main>
-        </body>
+            <section className="mt-6">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                            Controle de Caixa
+                        </h3>
+                        <p className="text-muted-foreground text-sm">Gerencie suas transações e acompanhe o fluxo de caixa</p>
+                    </div>
+                    <div>
+                        <Button className={`${open ? "bg-red-600 hover:bg-red-600" : "bg-green-500 hover:bg-green-500"}`} onClick={() => setOpen(!open)}>{open ? "Fechar Caixa" : "Abrir Caixa"}</Button>                    
+                    </div>
+                </div>
+                <div className="p-2">
+                    <p className={`text-xs ${open ? "text-green-500" : "text-muted-foreground"}`}><span className={`w-2 h-2 rounded-full mr-1 inline-block ${open ? "bg-green-500" : "bg-gray-500"}`}></span>{open ? "Caixa Aberto" : "Caixa Fechado"}</p>
+                </div>
+            </section>
+        </main>
     )
 }
